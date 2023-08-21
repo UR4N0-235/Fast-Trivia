2251e94632580002071b068682310a5a3fe08e90
UR4N0-235 <matheus.fernandes92.235@protonmail.com>
2023-08-21 15:15:06 -0300

    refactor: mock http + now get questionaries simulating a http request

--- lib/app/utils/http_request_mocked.dart
+++ lib/app/utils/http_request_mocked.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/testing.dart';
+import 'package:dio/dio.dart';
+import 'package:http_mock_adapter/http_mock_adapter.dart';
 
 class HttpRequestMocked {
   // TODO: implement mock http
   // static Future<String> loadJsonData() async {
   //   final response = await http.get(Uri.parse('assets/questions.json'));
   //   if (response.statusCode == 200) {
-  //       return json.decode(response.body);  
+  //       return json.decode(response.body);
   //   } else {
   //     throw Exception('Failed to load JSON');
   //   }
   // }
 
-  static loadJsonData() async{
-    return await rootBundle.loadString('assets/questionaries/all_questionaries.json');              
+  static Future<String> loadJsonData() async {
+    final dio = Dio(BaseOptions());
+    final dioAdapter = DioAdapter(dio: dio);
+
+    const path = 'https://example.com';
+
+    final mockedResponse = await rootBundle
+        .loadString('assets/questionaries/all_questionaries.json');
+
+    dioAdapter.onGet(
+      path,
+      (server) => server.reply(
+        200,
+        mockedResponse,
+        delay: const Duration(seconds: 1),
+      ),
+    );
+
+    final response = await dio.get(path);
+
+    print(response.data); // {message: Success!}
+    return response.data;
   }
 }
